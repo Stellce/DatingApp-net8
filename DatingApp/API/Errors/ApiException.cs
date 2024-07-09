089ecced79d990a39ebb8ed54e86d1a7c9fab96b
Stellce <andrey.is323@gmail.com>
2024-07-10 01:31:35 +0200

    Error handling. API Middleware. Angular Interceptors. Troubleshooting exceptions

---
+++ DatingApp/API/Errors/ApiException.cs
@@ -0,0 +1,9 @@
+﻿namespace API;
+
+public class ApiException(int statusCode, string message, string? details)
+{
+    public int StatusCode { get; set; } = statusCode;
+    public string message { get; set; } = message;
+    public string details { get; set; } = details;
+
+}
