64aa31d3ac4a62449515ec420d7fd44ad13cead0
Stellce <andrey.is323@gmail.com>
2024-07-12 18:42:46 +0200

    Implemented Cloudinary image support, photo upload and delete, setting main photo

--- DatingApp/API/Extensions/ApllicationServiceExtensions.cs
+++ DatingApp/API/Extensions/ApllicationServiceExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using API.Data;
 using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions;
@@ -17,7 +18,9 @@ public static class ApllicationServiceExtensions
         services.AddCors();
         services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IPhotoService, PhotoService>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
 
         return services;
     }
--- DatingApp/API/Services/TokenService.cs
+++ DatingApp/API/Services/TokenService.cs
@@ -5,7 +5,7 @@ using API.Entities;
 using API.Interfaces;
 using Microsoft.IdentityModel.Tokens;
 
-namespace API;
+namespace API.Services;
 
 public class TokenService(IConfiguration config) : ITokenService
 {
