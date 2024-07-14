42ec29ea5b6cf50de8868fb5640ffb41c50ec581
Stellce <andrey.is323@gmail.com>
2024-07-15 00:58:40 +0200

    Roles. Editing roles. Refactoring for .net Identity and role management, adding roles to jwt, policy based authorisation, admin guard, service, controller

--- DatingApp/client/src/app/_services/account.service.ts
+++ DatingApp/client/src/app/_services/account.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { User } from '../_models/user.model';
 import { map } from 'rxjs';
 import { environment } from '../../environments/environment';
@@ -13,6 +13,14 @@ export class AccountService {
   private likeService = inject(LikesService);
   baseUrl = environment.apiUrl;
   user = signal<User | null>(null);
+  roles = computed(() => {
+    const user = this.user();
+    if (user && user.token) {
+      const role = JSON.parse(atob(user.token.split('.')[1])).role
+      return Array.isArray(role) ? role : [role];
+    }
+    return [];
+  });
 
   login(user: {username: string, password: string}) {
     return this.http.post<User>(this.baseUrl + 'account/login', user).pipe(
