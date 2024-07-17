75c47616264a646a01b640b39cf33982f2963491
Stellce <andrey.is323@gmail.com>
2024-07-17 20:14:22 +0200

    Realtime messaging through WebSockets and SignalR. Using Hub, HubConnection, added PresenceTracker, PresenceHub, MessageHub, Groups, updated ssl

--- DatingApp/client/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp/client/src/app/members/member-messages/member-messages.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject, input, OnInit, output, viewChild } from '@angular/core';
-import { Message } from '../../_models/message.model';
+import { Component, inject, input, viewChild } from '@angular/core';
 import { MessageService } from '../../_services/message.service';
 import { TimeagoModule } from 'ngx-timeago';
 import { FormsModule, NgForm } from '@angular/forms';
@@ -12,19 +11,14 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './member-messages.component.css'
 })
 export class MemberMessagesComponent {
-  messageForm = viewChild.required<NgForm>('messageForm');
-  private messageService = inject(MessageService)
+  messageForm = viewChild<NgForm>('messageForm');
+  messageService = inject(MessageService)
   username = input.required<string>();
-  messages = input.required<Message[]>();
   messageContent = '';
-  updateMessages = output<Message>();
 
   sendMessage() {
-    this.messageService.sendMessage(this.username(), this.messageContent).subscribe({
-      next: message => {
-        this.updateMessages.emit(message);
-        this.messageForm().reset();
-      }
+    this.messageService.sendMessage(this.username(), this.messageContent).then(() => {
+      this.messageForm()?.reset();
     })
   }
 }
